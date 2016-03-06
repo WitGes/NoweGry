5cfa3d9b90dba177588286895b2672cb9016e801
WitGes <witek_gesse@poczta.onet.pl>
2016-03-06 13:34:27 +0100

    Dodałem tabelę

--- otello/dist/bundle.js
+++ otello/dist/bundle.js
@@ -44,10 +44,40 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(2)();
+	__webpack_require__(1)();
 
 /***/ },
 /* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $ = __webpack_require__(2);
+
+	module.exports=  function(){
+	    $(function(){
+	        var $container= $('<div>').attr('class','app');
+
+	        var $table=$('<table>');
+	        var $row=$('<tr>');
+	        var $cell = $('<td>');
+
+	        var size=8;
+
+	        for (var i =0;i<size;i++){
+
+	            for (var j=0;j<size;j++){
+	                 $row.append($cell);
+	            }
+	            $table.append($row);
+	        }
+	        $container.append($table);
+
+	        $('body').append($container);
+
+	    });
+	};
+
+/***/ },
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -9882,18 +9912,6 @@
 	return jQuery;
 	}));
 
-
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var $ = __webpack_require__(1);
-
-	module.exports=  function(){
-	    $(function(){
-	        $('body').append($('<div>').attr('id','app'));
-	    });
-	};
 
 /***/ }
 /******/ ]);
